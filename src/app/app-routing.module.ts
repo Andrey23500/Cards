d5df62cb0e17bc00732c9eba6fb2fb525feafc66
Andrey <andreyavs.00@gmail.com>
2023-07-11 11:13:55 +0300

    feat: add pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemComponent } from './pages/item/item.component';
+import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "main", pathMatch: "full" },
+  { path: "main", component: MainComponent },
+  { path: "item/:id", component: ItemComponent },
+
+  { path: "**", redirectTo: "/main" },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
