51b8aa2f54d6226a8b33f719abe36acceb31fe9e
Andrey <andreyavs.00@gmail.com>
2023-07-12 13:09:11 +0300

    feat: add item page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,27 @@ import { AppComponent } from './app.component';
 import { ItemComponent } from './pages/item/item.component';
 import { MainComponent } from './pages/main/main.component';
 import { ButtonModule } from 'primeng/button';
-import { DataViewModule, DataViewLayoutOptions } from 'primeng/dataview';
+import { DataViewModule } from 'primeng/dataview';
+import { PanelModule } from 'primeng/panel';
+import { AccordionModule } from 'primeng/accordion';
+import { DividerModule } from 'primeng/divider';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemComponent,
     MainComponent
-
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     ButtonModule,
-    DataViewModule
+    DataViewModule,
+    PanelModule,
+    AccordionModule,
+    DividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
